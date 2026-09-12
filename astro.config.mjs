89e92adf394c7fcd5159da063718eb0d4848decf
Said <SaidDino01@gmail.com>
2026-09-11 22:14:12 -0500

    chupala brandon

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://slimergy.mx',
   integrations: [tailwind()],
   server: { host: true },
 });
